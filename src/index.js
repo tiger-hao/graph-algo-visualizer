be2ea260e084ce1c6a94e6c3ef140b1c1cfcb924
Tiger Hao <tigerhao@live.ca>
2019-09-15 17:52:47 -0400

    create store module

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
 
 import './index.css';
 import App from './App';
-import rootReducer from 'reducers';
-
-const store = createStore(rootReducer);
+import store from 'store';
 
 render(
   <Provider store={store}>
